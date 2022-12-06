3df388095546c903d0f0434547383fcbf577b4d4
Ole <ole.dening@gmail.com>
2022-12-06 14:46:35 +0100

    updated team section

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,3 +1,6 @@
+import { AtSymbolIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/solid";
+
 const people = [
   {
     name: "Johann D.H. Harms",
@@ -6,23 +9,18 @@ const people = [
       "https://media-exp1.licdn.com/dms/image/C5603AQEFlp0-SEIxqA/profile-displayphoto-shrink_800_800/0/1553265639188?e=1675900800&v=beta&t=y7vnHanhadkKvTfTmZfKyZ_Gh4fU4H3iVpwZEZNSLmE",
     twitterUrl: "#",
     linkedinUrl: "https://www.linkedin.com/in/johann-h-60a378139/",
+    email: "harms.j@lippold.de",
   },
   {
-    name: "Herr Kube",
-    role: "Technischer Leiter",
+    name: "Thorsten Kube",
+    role: "Technischer Geschäftsführer",
     imageUrl:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGbg5HIshkL5Q/profile-displayphoto-shrink_800_800/0/1612850916036?e=1675900800&v=beta&t=M51cxwdSb_l7vdwZ4Pt1XOkIyNbrrMEc6qOtRfSMo-s",
+      "https://www.lippold.de/wp-content/uploads/2019/02/lippold_assets_kontakt_kube_300x300.jpg",
     twitterUrl: "#",
-    linkedinUrl: "https://www.linkedin.com/in/johann-h-60a378139/",
+    linkedinUrl: "#",
+    email: "kube@lippold.de",
   },
-  {
-    name: "Bernd Hansen",
-    role: "Steigbügelhalter",
-    imageUrl:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEJpHsqkhKdEg/profile-displayphoto-shrink_800_800/0/1623673652985?e=1675900800&v=beta&t=tsu4gYBxJMOG5b57_miJqeN77IwDsCqqHy_ueRYWzVU",
-    twitterUrl: "#",
-    linkedinUrl: "https://www.linkedin.com/in/johann-h-60a378139/",
-  },
+
   // More people...
 ];
 
@@ -57,7 +55,19 @@ export default function AboutUs() {
                       <h3>{person.name}</h3>
                       <p className="text-indigo-600">{person.role}</p>
                     </div>
-                    <ul role="list" className="flex justify-center space-x-5">
+                    <ul
+                      role="list"
+                      className="flex justify-center items-center space-x-5"
+                    >
+                      <li>
+                        <a
+                          href={`mailto:${person.email}`}
+                          className="text-gray-400 hover:text-gray-500"
+                        >
+                          <span className="sr-only">LinkedIn</span>
+                          <EnvelopeIcon className="w-6 h-6" />
+                        </a>
+                      </li>
                       <li>
                         <a
                           href={person.linkedinUrl}
