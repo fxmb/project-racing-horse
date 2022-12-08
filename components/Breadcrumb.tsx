2db3feae5ad81a284b54e731e174aa7795e34d6d
Ole <ole.dening@gmail.com>
2022-12-08 11:24:44 +0100

    reversed the parents order

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -21,29 +21,32 @@ export default function Breadcrumb({
             </Link>
           </div>
         </li>
-        {Object.entries(product_parents).map(([key, value]) => (
-          <li key={key} className="flex">
-            <div className="flex items-center">
-              <svg
-                className="h-full w-6 flex-shrink-0 text-gray-200"
-                viewBox="0 0 24 44"
-                preserveAspectRatio="none"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-              </svg>
-              <Link
-                href={`/#`}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 "
-                aria-current={key ? "page" : undefined}
-              >
-                {value}
-              </Link>
-            </div>
-          </li>
-        ))}
+        {Object.entries(product_parents)
+          .sort()
+          .reverse()
+          .map(([key, value]) => (
+            <li key={key} className="flex">
+              <div className="flex items-center">
+                <svg
+                  className="h-full w-6 flex-shrink-0 text-gray-200"
+                  viewBox="0 0 24 44"
+                  preserveAspectRatio="none"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+                <Link
+                  href={`/#`}
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 "
+                  aria-current={key ? "page" : undefined}
+                >
+                  {value}
+                </Link>
+              </div>
+            </li>
+          ))}
       </ol>
     </nav>
   );
