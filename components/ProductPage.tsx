2101a375105d61c78d34858bba4ca6d391d51a0f
Ole <ole.dening@gmail.com>
2022-12-08 11:40:46 +0100

    made badges bigger

--- components/ProductPage.tsx
+++ components/ProductPage.tsx
@@ -104,22 +104,22 @@ export default function ProductPage({ product }: { product: Product }) {
               <div className="mt-3">
                 <h2 className="sr-only">Product information</h2>
 
-                <p className="text-sm tracking-tight text-gray-900 mt-2">
+                <p className="text-md tracking-tight text-gray-900 mt-2">
                   Fremdartikelnummer:{" "}
-                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">
                     {product.product_details.manufacturer_type_descr}
                   </span>
                 </p>
-                <p className="text-sm tracking-tight text-gray-900 mt-2">
+                <p className="text-md tracking-tight text-gray-900 mt-2">
                   GTIN:
-                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">
                     {product.supplier_pid[1]["#text"]}
                   </span>
                 </p>
-                <p className="text-sm tracking-tight text-gray-900 mt-2">
+                <p className="text-md tracking-tight text-gray-900 mt-2">
                   {product.product_features[1].reference_feature_group_name}{" "}
                   Produkt ID:
-                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                  <span className="ml-1 inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">
                     {product.supplier_pid[0]["#text"]}
                   </span>
                 </p>
